86df6da596ca1cb03009f62f9f937f06b6a579ba
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-05-03 10:51:38 +1000

    fix mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="head_text text-center dark:text-white">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="purple_gradient text-center">AI-Powered Prompts</span>
+        <span className="purple_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Prompts is an open source tool to discover and share prompts
